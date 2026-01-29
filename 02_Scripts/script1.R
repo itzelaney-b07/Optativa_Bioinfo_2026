868414feb6b19c18e42849e3dfe56f143efc8d54
itzelaney-b07 <inovella07@alumnos.uaq.mx>
2026-01-29 16:58:39 -0600

    Initial commit

--- 02_Scripts/script1.R
+++ 02_Scripts/script1.R
@@ -1,9 +1,19 @@
 x<-rnorm(1000)
 hist(x)
 pdf("03_Results/mi_histograma_feo.pdf")
-hist(x,color="yellow4")
+hist(x,color="green2")
 boxplot(x)
 dev.off()
 
-
 usethis::use_git()
+
+0#| eval: false
+usethis::create_github_token()
+#| eval: false
+gitcreds::gitcreds_set()
+
+usethis::use_git_config(
+  user.name = "itzelaney-b07",
+  user.email = "inovella07@alumnos.uaq.mx"
+)
+usethis::use_github()
